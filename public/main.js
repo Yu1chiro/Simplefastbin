f6b77634cefd5eb4edd0ee86c92fe6cb271ba247
HeraBeginners25 <gallerysingaraja8@gmail.com>
2025-04-02 13:19:25 +0800

    obcus client

--- public/main.js
+++ public/main.js
@@ -1,135 +1,218 @@
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCNM6jFlIQJWn49pawqIulqW67FD8OSK6U",
-    authDomain: "bot-project-8d259.firebaseapp.com",
-    databaseURL: "https://bot-project-8d259-default-rtdb.firebaseio.com",
-    projectId: "bot-project-8d259",
-    storageBucket: "bot-project-8d259.firebasestorage.app",
-    messagingSenderId: "289445545944",
-    appId: "1:289445545944:web:3356b5213b45780ef394ad",
-    measurementId: "G-L1PQXGH3K3"
+  apiKey: "AIzaSyCNM6jFlIQJWn49pawqIulqW67FD8OSK6U",
+  authDomain: "bot-project-8d259.firebaseapp.com",
+  databaseURL: "https://bot-project-8d259-default-rtdb.firebaseio.com",
+  projectId: "bot-project-8d259",
+  storageBucket: "bot-project-8d259.firebasestorage.app",
+  messagingSenderId: "289445545944",
+  appId: "1:289445545944:web:3356b5213b45780ef394ad",
+  measurementId: "G-L1PQXGH3K3",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+function _0x2aef() {
+  const _0x253ce6 = [
+    "4446DKIRqU",
+    "text-color",
+    "location",
+    "<i\x20class=\x22fas\x20fa-save\x20mr-2\x22></i>Save",
+    "7596EilUar",
+    "foreColor",
+    "target",
+    "querySelectorAll",
+    "then",
+    "success",
+    "input",
+    "getElementById",
+    "[data-command^=\x22justify\x22]",
+    "stringify",
+    "#editor-toolbar\x20button[data-command]",
+    "json",
+    "addEventListener",
+    "71358iCcJNr",
+    "remove",
+    "#16a34a",
+    "getAttribute",
+    "top-end",
+    "Save\x20Confirmed!",
+    "10AEjtPZ",
+    "catch",
+    "trim",
+    "toggle",
+    "innerHTML",
+    "bg-blue-100",
+    "Error\x20saving\x20article:",
+    "application/json",
+    "message",
+    "contentEditable",
+    "150159iKFpEJ",
+    "32YhRvVY",
+    "value",
+    "highlight-color",
+    "fire",
+    "title-input",
+    "hidden",
+    "focus",
+    "error",
+    "hiliteColor",
+    "href",
+    "<i\x20class=\x22fas\x20fa-spinner\x20fa-spin\x20mr-2\x22></i>Saving...",
+    "21JGcCMt",
+    "2484867wfLUZu",
+    "325323aYThAB",
+    "Swal",
+    "forEach",
+    "execCommand",
+    "Please\x20enter\x20some\x20content!",
+    "data-command",
+    "classList",
+    "disabled",
+    "/api/save",
+    "7992TOuSSR",
+    "article-title",
+    "justify",
+    "DOMContentLoaded",
+    "Save\x20failed",
+    "save-btn",
+    "170ntVeXt",
+    "93814psyXOL",
+    "140vKwjsw",
+    "Failed\x20to\x20save",
+    "finally",
+    "click",
+    "url",
+    "#dc2626",
+  ];
+  _0x2aef = function () {
+    return _0x253ce6;
   };
-  
-  firebase.initializeApp(firebaseConfig);
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    const editor = document.getElementById('editor');
-    const saveBtn = document.getElementById('save-btn');
-    const titleInput = document.getElementById('title-input');
-    const articleTitle = document.getElementById('article-title');
-    
-    // Load SweetAlert2
-    const Swal = window.Swal;
-    
-    // Set up toolbar buttons
-    document.querySelectorAll('#editor-toolbar button[data-command]').forEach(button => {
-      button.addEventListener('click', () => {
-        const command = button.getAttribute('data-command');
-        
-        if (command === 'foreColor') {
-          document.getElementById('text-color').click();
-        } else if (command === 'hiliteColor') {
-          document.getElementById('highlight-color').click();
-        } else if (command.startsWith('justify')) {
-          // Handle text alignment
-          document.execCommand(command, false, null);
-          updateAlignmentButtons(command);
-        } else {
-          document.execCommand(command, false, null);
+  return _0x2aef();
+}
+const _0x5dae07 = _0x5d5a;
+function _0x5d5a(_0x1b5a62, _0x3417f6) {
+  const _0x2aefb0 = _0x2aef();
+  return (
+    (_0x5d5a = function (_0x5d5a00, _0x3f15ad) {
+      _0x5d5a00 = _0x5d5a00 - 0x189;
+      let _0x41cf4d = _0x2aefb0[_0x5d5a00];
+      return _0x41cf4d;
+    }),
+    _0x5d5a(_0x1b5a62, _0x3417f6)
+  );
+}
+(function (_0x49c206, _0x161c13) {
+  const _0x1257e2 = _0x5d5a,
+    _0x3d5037 = _0x49c206();
+  while (!![]) {
+    try {
+      const _0x22a59a =
+        -parseInt(_0x1257e2(0x1ab)) / 0x1 +
+        (-parseInt(_0x1257e2(0x1c9)) / 0x2) * (-parseInt(_0x1257e2(0x1b7)) / 0x3) +
+        (-parseInt(_0x1257e2(0x1c2)) / 0x4) * (parseInt(_0x1257e2(0x1c8)) / 0x5) +
+        (parseInt(_0x1257e2(0x19b)) / 0x6) * (parseInt(_0x1257e2(0x1ca)) / 0x7) +
+        (parseInt(_0x1257e2(0x1ac)) / 0x8) * (-parseInt(_0x1257e2(0x1b9)) / 0x9) +
+        (-parseInt(_0x1257e2(0x1a1)) / 0xa) * (parseInt(_0x1257e2(0x1b8)) / 0xb) +
+        (parseInt(_0x1257e2(0x18e)) / 0xc) * (parseInt(_0x1257e2(0x18a)) / 0xd);
+      if (_0x22a59a === _0x161c13) break;
+      else _0x3d5037["push"](_0x3d5037["shift"]());
+    } catch (_0x465476) {
+      _0x3d5037["push"](_0x3d5037["shift"]());
+    }
+  }
+})(_0x2aef, 0x2f647),
+  document[_0x5dae07(0x19a)](_0x5dae07(0x1c5), () => {
+    const _0x31646a = _0x5dae07,
+      _0x29f56b = document[_0x31646a(0x195)]("editor"),
+      _0x342c74 = document[_0x31646a(0x195)](_0x31646a(0x1c7)),
+      _0x3de18b = document[_0x31646a(0x195)](_0x31646a(0x1b0)),
+      _0x3fa8ef = document[_0x31646a(0x195)](_0x31646a(0x1c3)),
+      _0x45455b = window[_0x31646a(0x1ba)];
+    document["querySelectorAll"](_0x31646a(0x198))[_0x31646a(0x1bb)]((_0x399bc5) => {
+      const _0x4de008 = _0x31646a;
+      _0x399bc5[_0x4de008(0x19a)](_0x4de008(0x1cd), () => {
+        const _0x31bc80 = _0x4de008,
+          _0x37ccdc = _0x399bc5[_0x31bc80(0x19e)]("data-command");
+        if (_0x37ccdc === _0x31bc80(0x18f)) document["getElementById"](_0x31bc80(0x18b))["click"]();
+        else {
+          if (_0x37ccdc === "hiliteColor") document[_0x31bc80(0x195)]("highlight-color")["click"]();
+          else _0x37ccdc["startsWith"](_0x31bc80(0x1c4)) ? (document[_0x31bc80(0x1bc)](_0x37ccdc, ![], null), _0x27d199(_0x37ccdc)) : document["execCommand"](_0x37ccdc, ![], null);
         }
       });
-    });
-    
-    // Set up color pickers
-    document.getElementById('text-color').addEventListener('input', (e) => {
-      document.execCommand('foreColor', false, e.target.value);
-    });
-    
-    document.getElementById('highlight-color').addEventListener('input', (e) => {
-      document.execCommand('hiliteColor', false, e.target.value);
-    });
-    
-    // Save button click handler
-    saveBtn.addEventListener('click', saveArticle);
-    
-    function updateAlignmentButtons(activeCommand) {
-      document.querySelectorAll('[data-command^="justify"]').forEach(btn => {
-        btn.classList.toggle('bg-blue-100', btn.getAttribute('data-command') === activeCommand);
+    }),
+      document["getElementById"](_0x31646a(0x18b))[_0x31646a(0x19a)](_0x31646a(0x194), (_0x4f2736) => {
+        const _0x345884 = _0x31646a;
+        document["execCommand"](_0x345884(0x18f), ![], _0x4f2736[_0x345884(0x190)][_0x345884(0x1ad)]);
+      }),
+      document[_0x31646a(0x195)](_0x31646a(0x1ae))[_0x31646a(0x19a)](_0x31646a(0x194), (_0xf4ef58) => {
+        const _0x52b60f = _0x31646a;
+        document[_0x52b60f(0x1bc)](_0x52b60f(0x1b4), ![], _0xf4ef58[_0x52b60f(0x190)][_0x52b60f(0x1ad)]);
+      }),
+      _0x342c74[_0x31646a(0x19a)](_0x31646a(0x1cd), _0xf66909);
+    function _0x27d199(_0x587128) {
+      const _0x1800dd = _0x31646a;
+      document[_0x1800dd(0x191)](_0x1800dd(0x196))[_0x1800dd(0x1bb)]((_0x14b1ee) => {
+        const _0x13e83c = _0x1800dd;
+        _0x14b1ee["classList"][_0x13e83c(0x1a4)](_0x13e83c(0x1a6), _0x14b1ee[_0x13e83c(0x19e)](_0x13e83c(0x1be)) === _0x587128);
       });
     }
-    
-    function setupEditor() {
-      titleInput.classList.remove('hidden');
-      saveBtn.classList.remove('hidden');
-      editor.contentEditable = true;
-      editor.focus();
+    function _0x549e72() {
+      const _0x141e7e = _0x31646a;
+      _0x3de18b[_0x141e7e(0x1bf)]["remove"](_0x141e7e(0x1b1)), _0x342c74[_0x141e7e(0x1bf)][_0x141e7e(0x19c)](_0x141e7e(0x1b1)), (_0x29f56b[_0x141e7e(0x1aa)] = !![]), _0x29f56b[_0x141e7e(0x1b2)]();
     }
-    
-    function saveArticle() {
-        const content = editor.innerHTML;
-        const title = articleTitle.value.trim();
-        const pin = document.getElementById('article-pin').value.trim();
-        
-        if (!content) {
-          Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Please enter some content!',
-          });
-          return;
-        }
-        
-        saveBtn.disabled = true;
-        saveBtn.innerHTML = '<i class="fas fa-spinner fa-spin mr-2"></i>Saving...';
-        
-        fetch('/api/save', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ content, title, pin: pin || undefined }),
-        })
-        .then(response => response.json())
-        .then(data => {
-          if (data.success) {
-            Swal.fire({
-              toast: true,
-              position: 'top-end',
-              icon: 'success',
-              title: 'Save Confirmed!',
-              showConfirmButton: false,
-              timer: 1500,
-              timerProgressBar: true,
-              background: '#f0fdf4',
-              iconColor: '#16a34a'
-            }).then(() => {
-              window.location.href = data.url;
-            });
-          } else {
-            throw new Error(data.error || 'Failed to save');
-          }
-        })
-        .catch(error => {
-          console.error('Error saving article:', error);
-          Swal.fire({
-            toast: true,
-            position: 'top-end',
-            icon: 'error',
-            title: 'Save failed',
-            text: error.message,
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            background: '#fef2f2',
-            iconColor: '#dc2626'
-          });
-        })
-        .finally(() => {
-          saveBtn.disabled = false;
-          saveBtn.innerHTML = '<i class="fas fa-save mr-2"></i>Save';
-        });
+    function _0xf66909() {
+      const _0x2a5c89 = _0x31646a,
+        _0x3eb824 = _0x29f56b[_0x2a5c89(0x1a5)],
+        _0x29dc89 = _0x3fa8ef[_0x2a5c89(0x1ad)][_0x2a5c89(0x1a3)](),
+        _0x212c05 = document["getElementById"]("article-pin")[_0x2a5c89(0x1ad)][_0x2a5c89(0x1a3)]();
+      if (!_0x3eb824) {
+        _0x45455b[_0x2a5c89(0x1af)]({ icon: _0x2a5c89(0x1b3), title: "Oops...", text: _0x2a5c89(0x1bd) });
+        return;
       }
-    
-    // Initialize editor
-    setupEditor();
+      (_0x342c74["disabled"] = !![]),
+        (_0x342c74[_0x2a5c89(0x1a5)] = _0x2a5c89(0x1b6)),
+        fetch(_0x2a5c89(0x1c1), { method: "POST", headers: { "Content-Type": _0x2a5c89(0x1a8) }, body: JSON[_0x2a5c89(0x197)]({ content: _0x3eb824, title: _0x29dc89, pin: _0x212c05 || undefined }) })
+          [_0x2a5c89(0x192)]((_0x579499) => _0x579499[_0x2a5c89(0x199)]())
+          [_0x2a5c89(0x192)]((_0x58ea73) => {
+            const _0x438424 = _0x2a5c89;
+            if (_0x58ea73["success"])
+              _0x45455b[_0x438424(0x1af)]({
+                toast: !![],
+                position: _0x438424(0x19f),
+                icon: _0x438424(0x193),
+                title: _0x438424(0x1a0),
+                showConfirmButton: ![],
+                timer: 0x5dc,
+                timerProgressBar: !![],
+                background: "#f0fdf4",
+                iconColor: _0x438424(0x19d),
+              })[_0x438424(0x192)](() => {
+                const _0x42b128 = _0x438424;
+                window[_0x42b128(0x18c)][_0x42b128(0x1b5)] = _0x58ea73[_0x42b128(0x1ce)];
+              });
+            else throw new Error(_0x58ea73[_0x438424(0x1b3)] || _0x438424(0x1cb));
+          })
+          [_0x2a5c89(0x1a2)]((_0x339030) => {
+            const _0x2b1575 = _0x2a5c89;
+            console[_0x2b1575(0x1b3)](_0x2b1575(0x1a7), _0x339030),
+              _0x45455b["fire"]({
+                toast: !![],
+                position: _0x2b1575(0x19f),
+                icon: _0x2b1575(0x1b3),
+                title: _0x2b1575(0x1c6),
+                text: _0x339030[_0x2b1575(0x1a9)],
+                showConfirmButton: ![],
+                timer: 0xbb8,
+                timerProgressBar: !![],
+                background: "#fef2f2",
+                iconColor: _0x2b1575(0x189),
+              });
+          })
+          [_0x2a5c89(0x1cc)](() => {
+            const _0x93e5b3 = _0x2a5c89;
+            (_0x342c74[_0x93e5b3(0x1c0)] = ![]), (_0x342c74[_0x93e5b3(0x1a5)] = _0x93e5b3(0x18d));
+          });
+    }
+    _0x549e72();
   });
